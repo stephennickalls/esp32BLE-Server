d8e6955c03892c574dcb090981318fcf2f25ab52
stephennickalls <stephennickalls@gmail.com>
2023-11-28 19:38:41 +1300

    Add BLE service window, and deep sleep

--- esp32BLE-Server.ino
+++ esp32BLE-Server.ino
@@ -20,12 +20,11 @@ bool oldDeviceConnected = false;
 // timer, wake, and data transmission variables
 bool transmitData = false;
 int awakeTimeCounter = 0;
-const unsigned long AWAKETIME = 1 * 30 * 1000;
+const unsigned long AWAKETIME = 30;  // this will represent seconds;
 
 // EEPROM variables
 int currentCycleNumber;
-int awakeCycleNumber;
-const int maxAwakeCycleNumber = 4;
+const int maxCycleNumber = 3;  // 0 index so == 4
 const int addr = 0; // memory address
 
 
@@ -40,6 +39,7 @@ DHT dht(DHTPIN, DHTTYPE);
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define DATA_CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
 
 
 class MyServerCallbacks: public BLEServerCallbacks {
@@ -62,24 +62,10 @@ void setEEPROM() {
   if (value < 0 || value > maxValue) {
     EEPROM.put(addr, value);
     EEPROM.commit();
-    Serial.println("EEPROM initialized with 0");
+    Serial.println("EEPROM initialized with 1");
   }
 }
 
-void connectDisconnect(){
-      // handle connection/disconnection
-    if (!deviceConnected && oldDeviceConnected) {
-        delay(500); // give the bluetooth stack the chance to get things ready
-        pServer->startAdvertising(); // restart advertising
-        Serial.println("start advertising");
-        oldDeviceConnected = deviceConnected;
-    }
-    // connecting
-    if (deviceConnected && !oldDeviceConnected) {
-        // do stuff here on connecting
-        oldDeviceConnected = deviceConnected;
-    }
-}
 
 
 void setup() {
@@ -93,8 +79,7 @@ void setup() {
   // initialize EEPROM
   setEEPROM();
 
-
-  // Create the BLE Device
+      // Create the BLE Device
   BLEDevice::init("ESP32");
 
   // Create the BLE Server
@@ -121,21 +106,33 @@ void setup() {
   pBLE2902->setNotifications(true);
   pCharacteristic->addDescriptor(pBLE2902);
 
-}
+
+pService->start();
+
+// Start advertising
+BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+pAdvertising->addServiceUUID(SERVICE_UUID);
+pAdvertising->setScanResponse(false);
+pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+BLEDevice::startAdvertising();
+Serial.println("BLE service started");
+
+} // end setup
+
 
 void loop() {
-    Serial.println("in loop");
-
     // read cycle number from EEPROM
     EEPROM.get(addr, currentCycleNumber);
 
-    Serial.print("Current cycle number is:");
+    Serial.print("Current cycle number with zero index is:");
     Serial.println(currentCycleNumber);
 
     // if this is the 4th cycle then transmit the data. 
     // otherwise we write to sd card and go back to sleep
-    if (currentCycleNumber == 4){
+    if (currentCycleNumber == maxCycleNumber ){
       transmitData = true;
+      Serial.print("transmit data set to: ");
+      Serial.println(transmitData);
     }
 
     // read data from the DHT sensor
@@ -155,50 +152,66 @@ void loop() {
           serializeJson(jsonDoc, jsonString);
           // Notify to connected clients
           if (transmitData) {
+            Serial.println("transmitData was true");
               // Start the ble service
-            pService->start();
-
-            // Start advertising
-            BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-            pAdvertising->addServiceUUID(SERVICE_UUID);
-            pAdvertising->setScanResponse(false);
-            pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
-            BLEDevice::startAdvertising();
-            Serial.println("Waiting a client connection to notify...");
-            
-            while (awakeTimeCounter >= AWAKETIME){
-              pCharacteristic->setValue(jsonString);
-              pCharacteristic->notify();
+            delay(500); // give the bluetooth stack the chance to get things ready
+            pServer->startAdvertising(); // restart advertising
+            Serial.println("start advertising");
+            Serial.print("awakeTimeCounter: ");
+            Serial.println(awakeTimeCounter);
+            Serial.print("AWAKETIME: ");
+            Serial.println(AWAKETIME);
+            while (awakeTimeCounter <= AWAKETIME){
+              Serial.print("awakeTimeCounter:");
+              Serial.println(awakeTimeCounter);
+              while (deviceConnected && (awakeTimeCounter <= AWAKETIME)) {
+                pCharacteristic->setValue(jsonString);
+                pCharacteristic->notify();
+                Serial.print("Notify time passed (sec): ");
+                Serial.println(awakeTimeCounter);
+                delay(1000); 
+                awakeTimeCounter++;
+              }
               // counting seconds for awake time timer
-              // TODO: implement a propper timer.
+              Serial.println("waiting for hub to connect");
               delay(1000); 
               awakeTimeCounter++;
-              Serial.print("Notify time passed (sec): ");
-              Serial.println(awakeTimeCounter);
+              
             }
             // TODO: if success here then delete data and go back to sleep, maybe sync timer
             Serial.println("delete data from SD card here");
-            awakeCycleNumber = 0;  
+            currentCycleNumber = 0;  
             // write awake cule number to EEPROM
-            EEPROM.put(addr, awakeCycleNumber);
+            EEPROM.put(addr, currentCycleNumber);
             EEPROM.commit();
             esp_deep_sleep_start(); 
 
           } else {
-            awakeCycleNumber = (currentCycleNumber + 1) % (maxAwakeCycleNumber + 1); // increment the number of awake cycles we have had and set to 0 if more than 4 
+            currentCycleNumber = (currentCycleNumber + 1) % (maxCycleNumber + 1); // increment the number of awake cycles we have had and set to 0 if more than 3 
             // TODO: save data then sleep untill next cycle
             Serial.println("Saving data to SD card here");
-            EEPROM.put(addr, awakeCycleNumber);
+            EEPROM.put(addr, currentCycleNumber);
             EEPROM.commit();
             esp_deep_sleep_start(); 
           }
         }
       Serial.println("skipped all the ifs and went right to the next cycle grrrr");
-      awakeCycleNumber = (currentCycleNumber + 1) % (maxAwakeCycleNumber + 1); // increment the number of awake cycles we have had and set to 0 if more than 4 
-      EEPROM.put(addr, awakeCycleNumber);
+      currentCycleNumber  = (currentCycleNumber + 1) % (maxCycleNumber + 1); // increment the number of awake cycles we have had and set to 0 if more than 3 
+      EEPROM.put(addr, currentCycleNumber );
       EEPROM.commit();
 
-    connectDisconnect();
+          // handle connection/disconnection
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
 
 
 }
